666f6ec9aa0a0396d930a081c13426dd55bacfb0
Patrick Jordan-Smith <patrick@jordan-smith.org>
2015-11-19 09:47:17 +1300

    Adding docs

--- doc.go
+++ doc.go
@@ -1,9 +1,4 @@
+// api is a package to provide some extras to the echo framework for building secured
+// API services. It provides an api key mechinism with various backends which can be
+// chained in with your middleware stack.
 package api
-
-/*
-
-api is a package to provide some extras to the echo framework for building secured
-API services. It provides an api key mechinism with various backends which can be
-chained in with your middleware stack.
-
-*/
